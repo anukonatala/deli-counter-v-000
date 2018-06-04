35176efbb334a44216a4b654358b055b68aa8a0d
Anusha Konatala <anu.konatala@gmail.com>
2018-06-04 05:47:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(persons)
   else
     string1 = "The line is currently:"
     string2 = ""
-    persons.each_with_index { |person,index| string2 << " #{index}. " << "#{person}"}
+    persons.each_with_index { |person,index| string2 << " #{index+1}. " << "#{person}"}
     string1 << string2
  end
 
